1c13ec4a0ff3eef9ca5729408c154c9a3067790d
Nick Mounts <nick.mounts@du.edu>
2025-05-17 03:23:25 +0000

    Fix to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-    base: 'https://cs-classroom-2023-comp-3410.github.io/project08-react-virtual-pet-Ndmounts/',
+    base: '/project08-react-virtual-pet-Ndmounts/',
     plugins: [react()],
     server: {
         port: 5173,
-        // You can change this if needed
     },
 });
